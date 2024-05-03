4c55a61859f61f585524f65ee280ac9ef71b8345
BoSeung <gda05189@gmail.com>
2024-05-03 23:19:01 +0900

    feat: UserService getUserInfo 메서드 추가
    - User 정보를 조회하는 메서드를 추가했습니다
    - 역직렬화를 통해 DB로부터 이미지 URL을 읽는 private 메서드를 추가했습니다

--- spring-server/aistyling/src/main/java/com/clothz/aistyling/api/service/user/UserService.java
+++ spring-server/aistyling/src/main/java/com/clothz/aistyling/api/service/user/UserService.java
@@ -1,14 +1,22 @@
 package com.clothz.aistyling.api.service.user;
 
 import com.clothz.aistyling.api.controller.dto.request.UserCreateRequest;
-import com.clothz.aistyling.api.service.user.response.UserResponse;
+import com.clothz.aistyling.api.service.user.response.UserInfoResponse;
+import com.clothz.aistyling.api.service.user.response.UserSingUpResponse;
 import com.clothz.aistyling.domain.user.User;
 import com.clothz.aistyling.domain.user.UserRepository;
 import com.clothz.aistyling.domain.user.constant.UserRole;
-import jakarta.transaction.Transactional;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional
@@ -16,11 +24,13 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final ObjectMapper objectMapper;
 
-    public UserResponse signUp(final UserCreateRequest request) {
+    public UserSingUpResponse signUp(final UserCreateRequest request) {
+        sameCheckEmail(request.email());
         final String encodePassword = passwordEncoder.encode(request.password());
         final User user = userRepository.save(createUserEntity(request, encodePassword));
-        return UserResponse.from(user);
+        return UserSingUpResponse.from(user);
     }
 
     private User createUserEntity(UserCreateRequest request, String encodePassword) {
@@ -30,5 +40,18 @@ public class UserService {
                 .password(encodePassword)
                 .userRole(UserRole.USER)
                 .build();
+    }
+
+    @Transactional(readOnly = true)
+    public UserInfoResponse getUserInfo(Long id) throws JsonProcessingException {
+        final User user = userRepository.findById(id).orElseThrow(
+                () -> new UsernameNotFoundException("User not found")
+        );
+        List<String> imgUrls = deserializeImageUrls(user);
+        return UserInfoResponse.of(user, imgUrls);
+    }
+
+    private List<String> deserializeImageUrls(User user) throws JsonProcessingException {
+        return objectMapper.readValue(user.getUserImages(), new TypeReference<List<String>>() {});
     }
 }

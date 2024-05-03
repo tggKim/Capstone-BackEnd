51aff6590752ebddbcc23e6c3b90c83ba35f8af4
BoSeung <gda05189@gmail.com>
2024-05-03 23:17:07 +0900

    feat: getUserInfo handler 추가
    - 유저의 정보를 조회할 수 있는 handler를 추가했습니다.

--- spring-server/aistyling/src/main/java/com/clothz/aistyling/api/controller/UserController.java
+++ spring-server/aistyling/src/main/java/com/clothz/aistyling/api/controller/UserController.java
@@ -3,14 +3,15 @@ package com.clothz.aistyling.api.controller;
 import com.clothz.aistyling.api.ApiResponse;
 import com.clothz.aistyling.api.controller.dto.request.UserCreateRequest;
 import com.clothz.aistyling.api.service.user.UserService;
-import com.clothz.aistyling.api.service.user.response.UserResponse;
+import com.clothz.aistyling.api.service.user.response.UserInfoResponse;
+import com.clothz.aistyling.api.service.user.response.UserSingUpResponse;
+import com.clothz.aistyling.global.jwt.userInfo.CustomUserDetails;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -19,8 +20,17 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ApiResponse<UserResponse> signUp(@RequestBody @Valid final UserCreateRequest request) {
-        var userResponse = userService.signUp(request);
+    public ApiResponse<UserSingUpResponse> signUp(@RequestBody @Valid final UserCreateRequest request) {
+        final var userResponse = userService.signUp(request);
+        return ApiResponse.ok(userResponse);
+    }
+
+    @GetMapping("/users")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public ApiResponse<UserInfoResponse> getUserInfo(
+            @AuthenticationPrincipal final CustomUserDetails userDetails
+    ) throws JsonProcessingException {
+        final var userResponse = userService.getUserInfo(userDetails.getId());
         return ApiResponse.ok(userResponse);
     }
 }

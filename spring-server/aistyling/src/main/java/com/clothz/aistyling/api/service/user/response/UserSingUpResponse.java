9fab0d791ef2f96e4e0c560fc755a052fd42014f
BoSeung <gda05189@gmail.com>
2024-05-03 23:15:50 +0900

    refactor: UserResponse 클래스 이름 변경
    - UserSignUpResponse로 이름을 변경했습니다.

---
+++ spring-server/aistyling/src/main/java/com/clothz/aistyling/api/service/user/response/UserSingUpResponse.java
@@ -0,0 +1,19 @@
+package com.clothz.aistyling.api.service.user.response;
+
+import com.clothz.aistyling.domain.user.User;
+import lombok.Builder;
+
+public record UserSingUpResponse(String email, String nickname) {
+    @Builder
+    public UserSingUpResponse(String email, String nickname){
+        this.email = email;
+        this.nickname = nickname;
+    }
+
+    public static UserSingUpResponse from(User user) {
+        return UserSingUpResponse.builder()
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .build();
+    }
+}
